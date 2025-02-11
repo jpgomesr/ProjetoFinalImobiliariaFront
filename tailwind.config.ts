9584019cfbe0999d4531353f1ff4653dab62170b
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-10 21:52:05 -0300

    feat: Projeto NextJS iniciado

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+export default {
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            background: "var(--background)",
+            foreground: "var(--foreground)",
+            havprincipal: "#702632",
+         },
+         fontSize: {
+            fontcard1: "1.6rem",
+         },
+         fontFamily: {
+            inter: "Inter",
+         },
+      },
+   },
+   plugins: [],
+} satisfies Config;
