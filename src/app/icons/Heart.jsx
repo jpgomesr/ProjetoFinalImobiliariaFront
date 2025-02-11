65194ff6b8d65a424bcc86570b1e2cfb7df55f01
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-11 18:05:34 -0300

    style: Icone heart modificado

--- src/app/icons/Heart.jsx
+++ src/app/icons/Heart.jsx
@@ -1,24 +1,22 @@
 import { useEffect, useState } from "react";
 
 function Heart({ favorited }) {
-   const [color, setColor] = useState(favorited ? "#702632" : "transparent");
-   const [stroke, setStroke] = useState(
+   const [color] = useState(favorited ? "#702632" : "transparent");
+   const [stroke] = useState(
       color == "transparent" ? "#000000" : "transparent"
    );
 
    return (
       <svg
          xmlns="http://www.w3.org/2000/svg"
-         width={30}
-         height={30}
-         viewBox="0 0 20 20"
+         width="1.8em"
+         height="1.8em"
+         viewBox="0 0 512 512"
       >
          <path
+            d="M256 448l-30.164-27.211C118.718 322.442 48 258.61 48 179.095 48 114.221 97.918 64 162.4 64c36.399 0 70.717 16.742 93.6 43.947C278.882 80.742 313.199 64 349.6 64 414.082 64 464 114.221 464 179.095c0 79.516-70.719 143.348-177.836 241.694L256 448z"
             fill={color}
-            fillRule="evenodd"
-            d="M10 5.722c1.69-3.023 7.5-1.968 7.5 2.4c0 2.918-2.5 5.582-7.5 7.993c-5-2.41-7.5-5.075-7.5-7.993c0-4.368 5.81-5.423 7.5-2.4Z"
-            clipRule="evenodd"
-            strokeWidth={0.5}
+            strokeWidth={10}
             stroke={stroke}
          ></path>
       </svg>
