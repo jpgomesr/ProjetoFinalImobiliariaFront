b212ec3001e26a0ed3ee923614f8a0786ff6a512
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-11 18:06:08 -0300

    test: Página de teste de componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,18 @@ import CardImovel from "./components/CardImovel";
 export default function Home() {
    return (
       <div>
-         <CardImovel tipo="Casa" favorited={false} />
+         <CardImovel
+            tipo="Casa"
+            favorited={false}
+            objImovel="Venda"
+            valor={999999}
+            qtdFav={9999}
+            codigo={12345}
+            endereco="Rua Arthur Gonçalves de Araujo"
+            bairro="João Pessoa"
+            cidade="Jaraguá do Sul"
+            descricao="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque illum, voluptas commodi ullam enim ipsam voluptate, cupiditate natus."
+         />
       </div>
    );
 }
