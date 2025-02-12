643018860ad5ff0a8faf7830ae34c0bd1dd94c84
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-12 18:38:56 -0300

    feat: Banner para o card criado, utilizando variáveis para considerar sua visibilidade e o texto escrito

---
+++ src/app/components/card/CardBanner.tsx
@@ -0,0 +1,13 @@
+interface HomeProps {
+   tipo: string;
+}
+
+export default function CardBanner(props: HomeProps) {
+   return (
+      <div className="absolute top-4 w-full">
+         <div className="flex justify-center items-center w-full bg-havprincipal">
+            <p className="py-1 text-white font-semibold text-lg">{props.tipo}</p>
+         </div>
+      </div>
+   );
+}
