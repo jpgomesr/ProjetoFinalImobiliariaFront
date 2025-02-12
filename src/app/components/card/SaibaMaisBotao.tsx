b23138e460360b958006eeed77f2d3bdc44d2a0b
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-12 18:22:23 -0300

    feat: Criado botão de saiba mais, ainda sem funcionalidade

---
+++ src/app/components/card/SaibaMaisBotao.tsx
@@ -0,0 +1,11 @@
+interface HomeProps {
+   codigo: number;
+}
+
+export default function SaibaMaisBotao({ codigo }: HomeProps) {
+   return (
+      <button className="bg-havprincipal rounded-md">
+         <p className="py-3 px-4 text-white text-sm">Saiba Mais</p>
+      </button>
+   );
+}
