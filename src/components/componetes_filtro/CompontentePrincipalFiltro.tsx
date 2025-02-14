f352b0ca1c3d3a459fe2953fdadef02758d200a3
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-14 13:56:33 -0300

    Chore: Estrutura de pastas atualizadas de acordo com o next 15

---
+++ src/components/componetes_filtro/CompontentePrincipalFiltro.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CompontentePrincipalFiltro = () => {
+  return (
+    <div className='h-[340px]'>
+
+    </div>
+  )
+}
+
+export default CompontentePrincipalFiltro
