4907cbc570e7094236e6777493dfbe51d73c854f
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-14 18:04:10 -0300

    feat: Header mobile iniciado

--- src/components/headers/HeaderVermelho.tsx
+++ src/components/headers/HeaderVermelho.tsx
@@ -2,8 +2,8 @@ import LogoHavClaro from "../../svg/logo/LogoHavClaro";
 
 export default function HeaderVermelho() {
    return (
-      <div className="bg-havprincipal px-6 py-2">
-         <LogoHavClaro />
+      <div className="bg-havprincipal px-6 flex justify-center flex-col">
+         <LogoHavClaro width={50} height={60} />
       </div>
    );
 }
