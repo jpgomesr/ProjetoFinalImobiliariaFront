8e50fec3462236d0b118c5f11125d45a90cdbacb
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-14 16:36:52 -0300

    chore:Configuração de variaveis de texto no tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,16 @@ export default {
             foreground: "var(--foreground)",
             havprincipal: "#702632",
             begepadrao: "#EDEDED",
+            vermelhoHav: "#6D2631",
+            cinzaNeutro: "#BCBCBC",
          },
          fontSize: {
             fontcard1: "1.6rem",
+            padraoLg: "1rem",
+            subtituloLg: "1.5rem",
+            padraoMd: "0.875rem",
+            subtitulolg: "1.5rem",
+            mobilePadrao: "0.75rem"
          },
          fontFamily: {
             inter: "Inter",
