836ddc451d4c854f8ea82aef2f52c42dd649f472
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-02-18 18:01:35 -0300

    chore: adicionadas as divs de titulo, logo e texto

--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -6,9 +6,17 @@ import React from "react";
 const page = () => {
    return (
       <Layout>
-         <div>
-            <h1 className="">Especialista no mercado imobiliário</h1>
-            <div className=" w-96 h-96 bg-red-950"></div>
+         <div className="px-5 flex items-center text-center flex-col">
+            <h1 className="">
+              Especialista no mercado imobiliário
+            </h1>
+            <p>
+               Lorem ipsum dolor sit amet. Aut praesentium voluptas est magnam
+               doloribus non sint neque ut officia voluptatem est amet
+               molestiae. Lorem ipsum dolor sit amet. Aut praesentium voluptas
+               est magnam doloribus non sint neque.
+            </p>
+            <div className=" w-96 h-96 bg-havprincipal"></div>
          </div>
       </Layout>
    );
