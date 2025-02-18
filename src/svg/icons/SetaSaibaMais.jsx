21d816a410af31f062361d031b91a317a42c33af
Carlos Augusto Kviatkowsky <147559517+CarlosCAK@users.noreply.github.com>
2025-02-18 16:03:13 -0300

    Revert "Feature/componente filtro"

--- src/svg/icons/SetaSaibaMais.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function LupaIcon(props) {
-    return (
-        <img style={{width: props.width, height: props.height, borderRadius: 30}} src="https://placehold.co/10x11" />
-    );
- }
