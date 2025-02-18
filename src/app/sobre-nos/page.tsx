3d7452ee87a8191368c6190b237ef02537026411
Pedro Gabriel Scholemberg <pedrogabrielsc1812@gmail.com>
2025-02-18 15:01:52 -0300

    chore: Iniciando sobre nos page

--- src/app/sobre-nos/page.tsx
+++ src/app/sobre-nos/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
+
+import Layout from "@/components/Layout";
+import React from "react";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+   return (
+      <Layout>
+         <div>
+            <h1 className="">Especialista no mercado imobiliário</h1>
+            <div className=" w-96 h-96 bg-red-950"></div>
+         </div>
+      </Layout>
+   );
+};
 
-export default page
+export default page;
