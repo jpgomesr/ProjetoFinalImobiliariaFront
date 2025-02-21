90d251aa8892f3d5f5eca9029128bdbc9dcc2052
Carlos Augusto Kviatkowsky <carloskviatkowsky@gmail.com>
2025-02-20 21:23:03 -0300

    feat:Iniciado o componente de card de usuário

--- src/components/CardUsuario.tsx
+++ src/components/CardUsuario.tsx
@@ -13,8 +13,22 @@ interface CardUsuarioProps {
 
 const CardUsuario = (props: CardUsuarioProps) => {
    return (
-      <div className="border border-gray-700 rounded-md shadow-[4px_4px_4px_rgba(0,0,0,0.2)] py-2 px-2 ">
+      <div className="text-sm border border-gray-700 rounded-md shadow-[4px_4px_4px_rgba(0,0,0,0.2)] py-2 px-2 flex gap-3">
          <FotoUsuarioDeslogado />
+         <div>
+            <p>Informações</p>
+
+            <div>
+               <div className="flex items-center justify-between">
+                  <p>Nome :{props.nome}</p>
+                  <p>Status :{props.status}</p>
+               </div>
+               <div className="flex items-center justify-between">
+                  <p>Email :{props.email}</p>
+                  <p>Tipo conta :{props.tipoConta}</p>
+               </div>
+            </div>
+         </div>
       </div>
    );
 };
