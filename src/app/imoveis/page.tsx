261ff23509fd16a60d489306632052b7eb728d0b
Carlos Augusto Kviatkowsky <147559517+CarlosCAK@users.noreply.github.com>
2025-03-30 23:01:04 -0300

    feat: Criado sistema de imóveis favoritados

--- src/app/imoveis/page.tsx
+++ src/app/imoveis/page.tsx
@@ -3,14 +3,68 @@ import Layout from "@/components/layout/LayoutPadrao";
 import SubLayoutPaginasCRUD from "@/components/layout/SubLayoutPaginasCRUD";
 import FiltrosImoveis from "./FiltrosImoveis";
 import ImoveisView from "./ImoveisView";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { buscarTodosImoveis } from "@/Functions/imovel/buscaImovel";
 
-const Page = () => {
+interface PageProps {
+   searchParams: Promise<{
+      precoMinimo?: string;
+      precoMaximo?: string;
+      metrosQuadradosMinimo?: string;
+      metrosQuadradosMaximo?: string;
+      quantidadeDeQuartos?: string;
+      quantidadeDeVagas?: string;
+      cidade?: string;
+      bairro?: string;
+      tipoImovel?: string;
+      finalidade?: string;
+      view?: string;
+   }>;
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+   const session = await getServerSession(authOptions);
+   const parametrosResolvidos = await searchParams;
+   const parametrosBusca = {
+      precoMinimo: parametrosResolvidos.precoMinimo ?? "0",
+      precoMaximo: parametrosResolvidos.precoMaximo ?? "0",
+      metrosQuadradosMinimo: parametrosResolvidos.metrosQuadradosMinimo ?? "0",
+      metrosQuadradosMaximo: parametrosResolvidos.metrosQuadradosMaximo ?? "0",
+      quantidadeDeQuartos: parametrosResolvidos.quantidadeDeQuartos ?? "0",
+      quantidadeDeVagas: parametrosResolvidos.quantidadeDeVagas ?? "0",
+      cidade: parametrosResolvidos.cidade ?? "",
+      bairro: parametrosResolvidos.bairro ?? "",
+      tipoImovel: parametrosResolvidos.tipoImovel ?? "",
+      finalidade: parametrosResolvidos.finalidade ?? "",
+   };
+   const view = parametrosResolvidos.view ?? "cards";
+
+   const { imoveis, pageableInfo, quantidadeElementos } =
+      await buscarTodosImoveis({
+         precoMinimo: parametrosBusca.precoMinimo,
+         precoMaximo: parametrosBusca.precoMaximo,
+         tamanhoMin: parametrosBusca.metrosQuadradosMinimo,
+         tamanhoMax: parametrosBusca.metrosQuadradosMaximo,
+         qtdQuartos: parametrosBusca.quantidadeDeQuartos,
+         qtdGaragens: parametrosBusca.quantidadeDeVagas,
+         cidade: parametrosBusca.cidade,
+         bairro: parametrosBusca.bairro,
+         tipoResidencia: parametrosBusca.tipoImovel,
+         finalidade: parametrosBusca.finalidade,
+         revalidate: 30,
+      });
    return (
       <Layout className="py-0">
          <SubLayoutPaginasCRUD>
             <FundoBrancoPadrao className="w-full" titulo="Imóveis Disponíveis">
                <FiltrosImoveis />
-               <ImoveisView />
+               <ImoveisView
+                  imoveis={imoveis}
+                  pageableInfo={pageableInfo}
+                  quantidadeElementos={quantidadeElementos}
+                  view={view}
+               />
             </FundoBrancoPadrao>
          </SubLayoutPaginasCRUD>
       </Layout>
--- src/components/FavBotao.tsx
+++ src/components/FavBotao.tsx
@@ -1,26 +1,75 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Heart from "../svg/icons/CoracaoIcon";
-
+import ModalCofirmacao from "./ComponentesCrud/ModalConfirmacao";
+import { useSession } from "next-auth/react";
+import { adicionarImovelFavorito, removerImovelFavorito } from "@/Functions/usuario/favoritos";
+import { useNotification } from "@/context/NotificationContext";
 interface HomeProps {
+   idImovel: number; 
    favorited: boolean;
    dark: boolean;
+   setIsFavorited: (favorited: boolean) => void;
 }
 
-export default function FavButton({ favorited, dark }: HomeProps) {
-   const [isFavorited, setIsFavorited] = useState(favorited);
+export default function FavButton({ idImovel, favorited, dark, setIsFavorited }: HomeProps) {
 
-   const handleChangeFav = () => {
-      setIsFavorited((prevState) => !prevState);
+   const [handleRemoveFav, setHandleRemoveFav] = useState(false);
+   const { data: session } = useSession();
+   const {showNotification} = useNotification()
+
+   // Atualiza o estado quando a prop favorited mudar
+   useEffect(() => {
+      setIsFavorited(favorited);
+   }, [favorited]);
+   
+   const removerFavorito = async (idImovel: number) => {
+      if (session?.user?.id) {
+         try {
+            await removerImovelFavorito(idImovel, session.user.id);
+            setHandleRemoveFav(false);
+            window.location.reload();
+            showNotification("Imóvel removido dos favoritos");
+         } catch (error) {
+            console.error("Erro ao remover favorito:", error);
+            showNotification("Erro ao remover favorito");
+         }
+      }
    };
 
+   const handleChangeFav = async () => {
+      if(!favorited){
+         try {
+            await adicionarImovelFavorito(idImovel, session?.user?.id ?? "");
+            setIsFavorited(true);
+            window.location.reload();
+            showNotification("Imóvel adicionado aos favoritos");
+         } catch (error) {
+            console.error("Erro ao adicionar favorito:", error);
+            showNotification("Erro ao adicionar aos favoritos");
+         }
+      } else {
+         setHandleRemoveFav(true);
+      }
+   };
+
+
    return (
+      <> 
       <button
          className="flex items-center justify-center"
          onClick={handleChangeFav}
       >
-         {<Heart favorited={isFavorited} height={15} width={15} dark={dark} />}
+         {<Heart favorited={favorited} height={15} width={15} dark={dark} />}
       </button>
+
+      <ModalCofirmacao
+         message="Deseja remover este imóvel dos seus favoritos?"
+         isOpen={handleRemoveFav}
+         onClose={() => setHandleRemoveFav(false)}
+         onConfirm={() => removerFavorito(idImovel)}
+      />
+      </>
    );
 }
